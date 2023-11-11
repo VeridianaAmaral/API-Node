6d32d4ded520b30681b85261935f9cec985c8e47
Veridiana Amaral <veridiana17timoteo@gmail.com>
2023-11-11 17:52:57 -0400

    [FEAT] Adição de anuncio
    * Criação da lógica de anuncio
    * Adição das rotas de anuncio
    * Criação da model de anuncio
    * Criação da controller de anuncio

--- src/routes/rota.js
+++ src/routes/rota.js
@@ -2,6 +2,7 @@ import express from "express";
 import usuario from "./usuario.js";
 import produto from "./produto.js";
 import venda from "./venda.js";
+import anuncio from "./anuncio.js";
 import {verifyToken as auth} from "../middleware/auth.js"
 
 const rota = express.Router();
@@ -10,9 +11,9 @@ console.log("[ROUTER] Starting routes....");
 rota.use("/usuario", usuario);
 rota.use("/produto", auth, produto);
 rota.use("/venda", auth, venda);
+rota.use("/anuncio", auth, anuncio);
 
 console.log("[ROUTER] Done!");
-
 
 export default rota;
 
